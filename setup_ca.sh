f9a74f95c4e21d67fb75ac35f7b99239b72f2421
Sam Gleske <sam.mxracer@gmail.com>
2014-03-06 23:22:43 -0500

    initial commit

---
+++ setup_ca.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#Script by Sam Gleske
+#Thu Mar  6 23:14:29 EST 2014
+#Ubuntu 13.10
+#Linux 3.11.0-12-generic x86_64
+#GNU bash, version 4.2.45(1)-release (x86_64-pc-linux-gnu)
+#Setup script has been adapted from instructions
+#http://www.g-loaded.eu/2005/11/10/be-your-own-ca/
+
+######################################################################
+#myCA is our Certificate Authority’s directory.
+#myCA/certs directory is where our server certificates will be placed.
+#myCA/newcerts directory is where openssl puts the created
+#   certificates in PEM (unencrypted) format and in the form 
+#   cert_serial_number.pem (eg 07.pem). Openssl needs this directory, 
+#   so we create it.
+#myCA/crl is where our certificate revokation list is placed.
+#myCA/private is the directory where our private keys are placed. Be 
+#   sure that you set restrictive permissions to all your private keys 
+#   so that they can be read only by root, or the user with whose 
+#   priviledges a server runs. If anyone steals your private keys, 
+#   then things get really bad.
+######################################################################
+mkdir -m 0755 -p ./myCA/private ./myCA/certs ./myCA/newcerts ./myCA/crl
+
+######################################################################
+#We are going to copy the default openssl configuration file 
+#   (openssl.cnf) to our CA’s directory.
+#On Ubuntu it is /etc/ssl; on Fedora it is /etc/pki/tls
+#This script assumes Ubuntu.
+cp /etc/ssl/openssl.cnf ./myCA/openssl.my.cnf
+
+######################################################################
+#This file does not need to be world readable, so we change its 
+#attributes.
+chmod 0600 ./myCA/openssl.my.cnf
+
+######################################################################
+#We also need to create two other files. This file serves as a 
+#database for openssl.
+touch ./myCA/index.txt
+
+######################################################################
+#The following file contains the next certificate’s serial number. 
+#Since we have not created any certificates yet, we set it to "01".
+echo '01' > ./myCA/serial
+
+######################################################################
+#Things to remember about SSL file extensions
+#  KEY – Private key (Restrictive permissions should be set on this)
+#  CSR – Certificate Request (This will be signed by our CA in order 
+#        to create the server certificates. Afterwards it is not 
+#        needed and can be deleted)
+#  CRT – Certificate (This can be publicly distributed)
+#  PEM – We will use this extension for files that contain both the 
+#        Key and the server Certificate (Some servers need this). 
+#        Permissions should be restrictive on these files.
+#  CRL – Certificate Revokation List (This can be publicly 
+#        distributed)
+
+
